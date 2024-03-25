6347fd399f043855cb2ac5a1adcbf1eb37828e48
Dharmesh Sharma <dharmesh.sharma@winjit.com>
2024-03-25 21:53:41 +0530

    Custom button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Button from './components/Button';
 
-
-function App() {
+const App = () => {
 
 
   return (
@@ -9,6 +10,10 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Button className="mt-10" href="#login" > Something</Button>
+      </div>
+      <ButtonGradient />
     </>
   )
 }
